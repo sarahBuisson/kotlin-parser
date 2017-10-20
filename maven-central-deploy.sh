58026cb425785294028093cd2d44680f3895a416
sbuisson <sbuisson@xebia.fr>
2017-10-21 00:09:32 +0200

    cdsc

---
+++ maven-central-deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Deploy maven artefact in current directory into Maven central repository
+# using maven-release-plugin goals
+
+read -p "Really deploy to maven central repository  (yes/no)? "
+
+if ( [ "$REPLY" == "yes" ] ) then
+  mvn -P release release:clean release:prepare release:perform -B -e | tee maven-central-deploy.log
+else
+  echo 'Exit without deploy'
+fi
